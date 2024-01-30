6f87121b0428c360b7e8782fd1e561149afad6f6
valeen-rodriguez <valentinarodriguezkittler@gmail.com>
2024-01-29 21:26:01 -0300

    Session & Cookies - Finalizado

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,25 +4,30 @@ const userController = {
     index: (req, res)=> {
         res.render ("index")
     },
+    
     login: (req, res) => {
-        const errors = validationResult(req);
+        const errors = validationResult(req.body);
         
         if (!errors.isEmpty()) {
-            return res.send("index", { errors: errors.mapped(), title: "Session&Cookies", old: req.body});
+            res.render("index", { errors: errors.mapped(), title: "Session&Cookies", old: req.body});
         }
 
         const user = req.body
-        console.log(user)
         req.session.user = user
         if (req.body.remember) {    
             res.cookie('user', user, { maxAge: 1000 * 60 * 15 });
             res.cookie('remember', user.remember, { maxAge: 1000 * 60 * 15 });
         }
 
-        res.send("index", {title: "Session", user: req.session.user});
+        res.render("index", {title: "Session", user: req.session.user});
     },
+
     mensaje: (req, res) => {
+        const user = req.session.user;
+        if (user && user.nombre)
+            res.render('mensaje', { title: 'Mensaje', user: user });
     },
+    
     logout: (req,res)=>{
         req.session.destroy();
         if (req.cookies.user) {
